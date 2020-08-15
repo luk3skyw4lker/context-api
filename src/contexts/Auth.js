85cfa38b7c5c8343cd304d5fb41457b9c584ffe8
luk3skyw4lker <luk3skyw4lker@github.com>
2020-08-14 21:30:12 -0300

    Adding AsyncStorage and SplashScreen logics

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import * as SplashScreen from "expo-splash-screen";
 
 import { createContext } from "react";
 import { signIn } from "../services/auth";
@@ -6,6 +8,7 @@ import { signIn } from "../services/auth";
 const AuthContext = createContext({
 	signed: true,
 	user: {},
+	loading: true,
 	login: () => {},
 	logout: () => {}
 });
@@ -13,10 +16,32 @@ const AuthContext = createContext({
 export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
 
+	useEffect(() => {
+		async function loadStoredData() {
+			const [user, token] = await AsyncStorage.multiGet([
+				"@RNAuth:user",
+				"@RNAuth:token"
+			]);
+
+			if (user && token) {
+				setUser(JSON.parse(user[1]));
+				if ((await SplashScreen.preventAutoHideAsync()) === true) {
+					console.log("HIDING");
+					await SplashScreen.hideAsync();
+				}
+			}
+		}
+
+		loadStoredData();
+	}, []);
+
 	async function login() {
 		const response = await signIn();
 
 		setUser(response.user);
+
+		await AsyncStorage.setItem("@RNAuth:user", JSON.stringify(response.user));
+		await AsyncStorage.setItem("@RNAuth:token", response.token);
 	}
 
 	function logout() {
